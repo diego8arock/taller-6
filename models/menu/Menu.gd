07451a01c1d10c3961e0eef4e787260a3b29c75b
diego8arock <diegoa2000alt@gmail.com>
2019-03-29 21:42:21 -0500

    Agregar menu rotatorio

--- models/menu/Menu.gd
+++ models/menu/Menu.gd
@@ -1,16 +1,76 @@
 extends Spatial
 
-export  (PackedScene) var panels
+export var panel : PackedScene = null
+export var partition : int = 3
+export var radius : int = 3
+export var scalar : float = 1.0
+var proportion : float = 0.03
+const DEGREES_CIRCLE : float = 360.0
 
+const dir_path = "res://assets/menu/"
+const dir_img = "images"
+const dir_video = "videos"
+const dir_model = "models"
+var total_files : int = 0
+
+var image_files = []
+var video_files = []
+var model_files = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var panel = panels.instance()
-	panel.transform.origin[0] = -1
-	$PanelContainer.add_child(panel)
+	read_files()
+	partition = total_files
 	
-	pass # Replace with function body.
+	for x in range(partition - 1):
+		scalar -= proportion
+	var vector_scalar = Vector3(scalar, scalar, scalar)
+	
+	var partition_angles : float = DEGREES_CIRCLE / partition	
+	var angle = 0
+	for x in range(partition):
+		var posx = radius * sin(to_radian(angle))
+		var posy = radius * cos(to_radian(angle))
+		angle += partition_angles
+		var panelNew = panel.instance()
+		$PanelContainer.add_child(panelNew)
+		panelNew.transform.origin[0] = posx
+		panelNew.transform.origin[1] = posy
+		var mesh = panelNew.get_node("Mesh")
+		mesh.transform = mesh.transform.scaled(vector_scalar)
+		
+	pass 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func to_radian(angle) -> float:
+	return angle * PI / 180
+	
+func read_files():
+	var dir = Directory.new()
+	if dir.open(dir_path + dir_img) == OK:
+		dir.list_dir_begin()
+		var file = dir.get_next()
+		while (file != ""):
+			if file.ends_with("jpeg") or file.ends_with("jpg"):
+				total_files += 1
+				image_files.append(file)
+			file = dir.get_next()
+			
+	dir = Directory.new()
+	if dir.open(dir_path + dir_video) == OK:
+		dir.list_dir_begin()
+		var file = dir.get_next()
+		while (file != ""):
+			if file.ends_with("webm"):
+				total_files += 1
+				video_files.append(file)
+			file = dir.get_next()
+			
+	dir = Directory.new()
+	if dir.open(dir_path + dir_model) == OK:
+		dir.list_dir_begin()
+		var file = dir.get_next()
+		while (file != ""):
+			if file.ends_with("tscn"):
+				total_files += 1
+				model_files.append(file)
+			file = dir.get_next()
