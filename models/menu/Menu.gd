5961a1754b8f31a4b6b86a1861686d0bc01552c1
diego8arock <diegoa2000alt@gmail.com>
2019-03-31 14:55:42 -0500

    Agregar materiales por tipo de archivo

--- models/menu/Menu.gd
+++ models/menu/Menu.gd
@@ -1,23 +1,32 @@
 extends Spatial
 
+#Enums
+enum PANEL_TYPE {
+		IMAGE,
+		VIDEO,
+		MODEL
+	}
+
+#Local export 
 export var panel : PackedScene = null
-export var partition : int = 3
 export var radius : int = 3
 export var scalar : float = 1.0
-var proportion : float = 0.03
+export var proportion : float = 0.03
 const DEGREES_CIRCLE : float = 360.0
 
+#Local constants
 const dir_path = "res://assets/menu/"
 const dir_img = "images"
 const dir_video = "videos"
 const dir_model = "models"
-var total_files : int = 0
 
+#Local varibales
+var total_files : int = 0
+var partition : int
 var image_files = []
 var video_files = []
 var model_files = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	read_files()
 	partition = total_files
@@ -28,6 +37,8 @@ func _ready():
 	
 	var partition_angles : float = DEGREES_CIRCLE / partition	
 	var angle = 0
+	var index_for_files = 0
+	var panel_type = PANEL_TYPE.IMAGE
 	for x in range(partition):
 		var posx = radius * sin(to_radian(angle))
 		var posy = radius * cos(to_radian(angle))
@@ -39,38 +50,42 @@ func _ready():
 		var mesh = panelNew.get_node("Mesh")
 		mesh.transform = mesh.transform.scaled(vector_scalar)
 		
+		index_for_files += 1	
+		match panel_type:
+			PANEL_TYPE.IMAGE:
+				if index_for_files > image_files.size():
+					panel_type = PANEL_TYPE.VIDEO
+					index_for_files = 1
+			PANEL_TYPE.VIDEO:
+				if index_for_files > video_files.size():
+					panel_type = PANEL_TYPE.MODEL
+					index_for_files = 1
+			PANEL_TYPE.MODEL:
+				if index_for_files > model_files.size():
+					panel_type = PANEL_TYPE.IMAGE
+					index_for_files = 1		
+		panelNew.set_panel_type(panel_type)
 	pass 
 
 func to_radian(angle) -> float:
 	return angle * PI / 180
 	
-func read_files():
+func read_files() -> void:
+	image_files = read_directory(dir_img, "jpg")
+	video_files = read_directory(dir_video, "webm")
+	model_files = read_directory(dir_model, "tscn")
+
+func read_directory(file_path, file_extension) -> Array:
+	var file_list = []
 	var dir = Directory.new()
-	if dir.open(dir_path + dir_img) == OK:
+	if dir.open(dir_path + file_path) == OK:
 		dir.list_dir_begin()
 		var file = dir.get_next()
 		while (file != ""):
-			if file.ends_with("jpeg") or file.ends_with("jpg"):
+			if file.ends_with(file_extension):
 				total_files += 1
-				image_files.append(file)
+				file_list.append(file)
 			file = dir.get_next()
-			
-	dir = Directory.new()
-	if dir.open(dir_path + dir_video) == OK:
-		dir.list_dir_begin()
-		var file = dir.get_next()
-		while (file != ""):
-			if file.ends_with("webm"):
-				total_files += 1
-				video_files.append(file)
-			file = dir.get_next()
-			
-	dir = Directory.new()
-	if dir.open(dir_path + dir_model) == OK:
-		dir.list_dir_begin()
-		var file = dir.get_next()
-		while (file != ""):
-			if file.ends_with("tscn"):
-				total_files += 1
-				model_files.append(file)
-			file = dir.get_next()
+	return file_list
+	
+	
---
+++ models/panel/Panel.gd
@@ -0,0 +1,27 @@
+extends KinematicBody
+
+enum PANEL_TYPE {
+		IMAGE,
+		VIDEO,
+		MODEL
+	}
+
+var type
+var image_material = preload("res://models/panel/ImageMaterial.tres")
+var video_material = preload("res://models/panel/VideoMaterial.tres")
+var model_material = preload("res://models/panel/ModelMaterial.tres")
+
+func _ready():
+	pass
+
+func set_panel_type(panel_type):
+	type = panel_type	
+	var mesh = $Mesh
+	match type:
+		PANEL_TYPE.IMAGE:
+			mesh.material_override = image_material
+		PANEL_TYPE.VIDEO:
+			mesh.material_override = video_material
+		PANEL_TYPE.MODEL:
+			mesh.material_override = model_material
+	
